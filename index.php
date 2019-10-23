1461529abcf0eb1bb7057902c718c1e0b6782dbe
sujuds <satwikayana@gmail.com>
2019-10-23 09:39:16 +0800

    detail profile

--- index.php
+++ index.php
@@ -48,8 +48,6 @@
         </div>
     </div>
 
-
-
     <!-- Modal -->
     <div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-scrollable modal-lg" role="document">
